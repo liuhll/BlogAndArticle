b11a4e2c47ca4d9dd25c8677d5942b6d4ef00b45
liuhl <1029765111@qq.com>
2016-10-20 20:47:55 +0800

    shell脚本的转义字符，命令替换，变量替换

---
+++ Notes/Linux/Shell脚本/Shell.Demo/variable.replace.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo ${var:-"Variable is not set"}
+echo "1 - Value of var is ${var}"
+
+echo ${var:="Variable is not set"}
+echo "2 - Value of var is ${var}"
+
+unset var
+echo ${var:+"This is default value"}
+echo "3 - Value of var is $var"
+
+var="Prefix"
+echo ${var:+"This is default value"}
+echo "4 - Value of var is $var"
+
+echo ${var:?"Print this message"}
+echo "5 - Value of var is ${var}"
+
+# 执行该脚本将输出一下内容：
+# ------------------------
+# Variable is not set
+#1 - Value of var is
+#Variable is not set
+#2 - Value of var is Variable is not set
+#3 - Value of var is
+#This is default value
+#4 - Value of var is Prefix
+#Prefix
+#5 - Value of var is Prefix
