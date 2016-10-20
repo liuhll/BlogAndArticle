295f88467f4e28c3135edac95be48f223fed40f5
liuhl <1029765111@qq.com>
2016-10-20 22:39:40 +0800

    shell脚本运算符笔记

---
+++ Notes/Linux/Shell脚本/Shell.Demo/arithmetic.demo.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+a=10
+b=20
+val=`expr $a + $b`
+echo "a + b : $val"
+val=`expr $a - $b`
+echo "a - b : $val"
+val=`expr $a \* $b`
+echo "a * b : $val"
+val=`expr $b / $a`
+echo "b / a : $val"
+val=`expr $b % $a`
+echo "b % a : $val"
+if [ $a == $b ]
+then
+   echo "a is equal to b"
+fi
+if [ $a != $b ]
+then
+   echo "a is not equal to b"
+fi
+
+# 运行该脚本输出
+# ------------------
+# a + b : 30
+# a - b : -10
+# a * b : 200
+# b / a : 2
+# b % a : 0
+# a is not equal to b
